3227006ddc01b44ce5f1373990f6134566a04e5d
iooogik <sbobrov760@gmail.com>
2019-12-31 22:59:34 +0300

    qr added

---
+++ app/src/main/java/com/example/squirrel/ShopActivity.java
@@ -0,0 +1,14 @@
+package com.example.squirrel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shop_activity);
+    }
+}
---
+++ app/src/main/java/com/example/squirrel/StandartNote.java
@@ -0,0 +1,14 @@
+package com.example.squirrel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StandartNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standart_note);
+    }
+}
