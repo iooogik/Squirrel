c6c7a89043611165720aba636fc8d4c7a08f415e
iooogik <sbobrov760@gmail.com>
2020-01-04 14:16:25 +0300

    notification added

--- app/src/main/java/com/example/squirrel/NotificationReceiver.java
+++ app/src/main/java/com/example/squirrel/NotificationReceiver.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 
