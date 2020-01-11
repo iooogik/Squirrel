efaf563b48a88bf1e107873146d11886f4701df3
iooogik <sbobrov760@gmail.com>
2020-01-11 15:04:07 +0300

    smth interesting

---
+++ app/src/main/java/com/example/squirrel/NoteInterface.java
@@ -0,0 +1,11 @@
+package com.example.squirrel;
+
+interface NoteInterface {
+    void updData(String databaseName, String name, String note, String shortNote);
+    String getBtnName();
+    int getBtnID();
+    void updFragment();
+    void updShopNotes(String databaseName, String name, String booleans);
+    void alarmDialog(final String title, final String text);
+
+}
